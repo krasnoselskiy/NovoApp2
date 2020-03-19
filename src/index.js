4e6b4b7abdeaf098c40c30028dc098c30759f55f
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-03-19 14:16:17 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store'
+import Routes from './routes'
+import './styles/globalStyles.css'
+import * as serviceWorker from './utils/serviceWorker'
+
+render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById('root'),
+)
+
+serviceWorker.unregister()
