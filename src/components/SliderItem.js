46cc68b5dc65c55d869d9bc142a69431e8009dce
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-03-23 00:36:43 +0200

    Added styles for slider item; added spinner

--- src/components/SliderItem.js
+++ src/components/SliderItem.js
@@ -1,14 +1,50 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import styled from '@emotion/styled';
 
 const SliderItem = ({ title, src, slug }) => {
   return (
-    <div className="slider-item">
-      <h3>{title}</h3>
+    <SliderItemWrap>
       <img src={src} alt={title} />
-      <Link to={'albums/' + slug }>See all photos</Link>
-    </div>
+      <Link className="link" to={'albums/' + slug }>Go to...</Link>
+    </SliderItemWrap>
   );
 }
+
+const SliderItemWrap = styled.div`
+  height: 100vh;
+  width: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  img {
+    object-fit: cover;
+    width: 100%;
+  }
+
+  .link {
+    position: absolute;
+    bottom: 70px;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    font-size: 20px;
+    z-index: 2;
+    color: #000;
+    padding: 12px;
+    display: block;
+    opacity: 0.2;
+    cursor: pointer;
+    text-decoration: none;
+    transition: all 0.35s;
+  }
+
+  .link:hover {
+    opacity: 1;
+  }
+`
+
+const Bounce = styled.div`animation: 2s ___CSS_0___ infinite`;
 
 export default SliderItem;
--- src/containers/AlbumListContainer.js
+++ src/containers/AlbumListContainer.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { connect } from "react-redux";
 import { fetchAlbums } from "../actions/albumActions";
 import Slider from "react-slick";
+import Loader from 'react-loader-spinner'
+import styled from '@emotion/styled'
+
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 
 import SliderItem from "../components/SliderItem";
 
@@ -21,8 +25,8 @@ class AlbumListContainer extends Component {
       arrows: true,
       infinite: true,
       fade: true,
-      speed: 2500,
-      autoplay: true,
+      speed: 1500,
+      autoplay: false,
       autoplaySpeed: 2000,
       slidesToShow: 1,
       slidesToScroll: 1
@@ -33,7 +37,9 @@ class AlbumListContainer extends Component {
     }
 
     if (loading) {
-      return <div>Loading...</div>;
+      return <SpinnerWrap>
+        <Loader type="MutatingDots" color="#000" timeout={3000} height={100} width={100} />
+      </SpinnerWrap>
     }
 
     return (
@@ -43,7 +49,7 @@ class AlbumListContainer extends Component {
             if (album.sys.contentType.sys.id == "photoGallery") {
               return <SliderItem key={album.sys.id}
                                 title={album.fields.title}
-                                src={album.fields.images[0].fields.photo.fields.file.url}
+                                src={album.fields.images[0].fields.imageList.fields.file.url}
                                 slug={album.fields.slug} />
             }
           }) : null}
@@ -58,5 +64,12 @@ const mapStateToProps = state => ({
   loading: state.albums.loading,
   error: state.albums.error
 });
+
+const SpinnerWrap = styled.div`
+ position: absolute;
+ top: 50%;
+ left: 50%;
+ transform: translate(-50%, -50%)
+`
 
 export default connect(mapStateToProps)(AlbumListContainer);
