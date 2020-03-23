1f1b63de804a46046351bb57429db830867cb20b
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-03-23 13:52:19 +0200

    Refactored album actions

--- src/actions/albumActions.js
+++ src/actions/albumActions.js
@@ -1,34 +1,24 @@
-import { ContentfulClient } from 'react-contentful';
-
-export const FETCH_ALBUMS_START = 'FETCH_ALBUMS_START';
-export const FETCH_ALBUMS_SUCCESS = 'FETCH_ALBUMS_SUCCESS';
-export const FETCH_ALBUMS_FAILURE = 'FETCH_ALBUMS_FAILURE';
-
-const client = new ContentfulClient({
-  accessToken: 'VyKxsscgd5-sp5Cwf3rFzbKjNx4im1t4cmewBJXC-kw',
-  space: 'po8v2ttqiqbn',
-});
+import { client } from './client';
+import ActionTypes from '../constants/actionTypes'
 
 export const fetchAlbumsStart = () => ({
-  type: FETCH_ALBUMS_START
+  type: ActionTypes.FETCH_ALBUMS_START
 });
 
 export const fetchAlbumsSuccess = items => ({
-  type: FETCH_ALBUMS_SUCCESS,
+  type: ActionTypes.FETCH_ALBUMS_SUCCESS,
   payload: { items }
 });
 
 export const fetchAlbumsFailure = error => ({
-  type: FETCH_ALBUMS_FAILURE,
+  type: ActionTypes.FETCH_ALBUMS_FAILURE,
   payload: { error }
 });
 
 export const fetchAlbums = () => dispatch => {
   dispatch(fetchAlbumsStart())
 
-  client.getEntries({
-
-  }).then((res) => {
+  client.getEntries().then((res) => {
     dispatch(fetchAlbumsSuccess(res.items))
   }).catch((err) => {
     dispatch(fetchAlbumsFailure(err))
