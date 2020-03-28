4c80b0a963b2fc6b36624d25f05d0baaf6c95f89
Boris Krasnoselskiy <boris.krasnoselskiy@gmail.com>
2020-03-28 16:31:53 +0200

    Minor fixes

--- src/actions/authorActions.js
+++ src/actions/authorActions.js
@@ -1,5 +1,6 @@
 import { client } from './client';
 import ActionTypes from '../constants/actionTypes'
+import { setThemeColor } from './themeActions';
 
 export const fetchAuthorStart = () => ({
   type: ActionTypes.FETCH_AUTHOR_START
@@ -20,7 +21,8 @@ const AUTHOR_ENTRY_ID = '4nUKMIa2GX30ohRtZvquT6';
 export const fetchAuthor = () => dispatch => {
   dispatch(fetchAuthorStart())
   client.getEntry(AUTHOR_ENTRY_ID).then((res) => {
-    dispatch(fetchAuthorSuccess(res.fields))
+    dispatch(setThemeColor('white'));
+    dispatch(fetchAuthorSuccess(res.fields));
   }).catch((err) => {
     dispatch(fetchAuthorFailure(err))
   });
